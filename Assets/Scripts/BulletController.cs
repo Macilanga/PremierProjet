df8c721af95f1369d68ea00fcebb10d61553f167
Macilanga <>
2020-10-15 21:39:58 +0200

    Initial commit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    private new Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
+        rigidbody2D.AddForce(new Vector2(0f, 200f));
+    }
+
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
